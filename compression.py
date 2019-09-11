065cf7b8eee547355df74c0094fc1de27c33bbdd
Sky Jones <rsajones94@gmail.com>
2019-09-11 14:11:05 -0500

    add recursive line analysis

--- compression.py
+++ compression.py
@@ -1,5 +1,8 @@
+from itertools import chain
+
 import numpy as np
 from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 
 def func_linear(x, m, b):
@@ -24,7 +27,29 @@ def get_r2(predicted, actual):
     return 1 - (sse / sst)
 
 
-def get_line(exes, whys, start_end):
+def sum_of_error(predicted, actual):
+    predicted = np.array(predicted)
+    actual = np.array(actual)
+
+    error = predicted-actual
+    abs_error = [abs(i) for i in error]
+    return sum(abs_error)
+
+
+def sum_of_square_error(predicted, actual):
+    predicted = np.array(predicted)
+    actual = np.array(actual)
+
+    error = predicted-actual
+    sq_error = [i**2 for i in error]
+    return sum(sq_error)
+
+
+def get_lin_values(exes, m, b):
+    return [func_linear(x, m, b) for x in exes]
+
+
+def get_line(exes, whys, start_end, return_error=False):
     """
     Returns m, b and r2 value for the line that best fits a subset of points.
 
@@ -33,8 +58,7 @@ def get_line(exes, whys, start_end):
         whys: the y values in the series
         start_end: a list of len 2 indicating the start (inclusive) and end (exclusive) of the subset.
                    Does not need to be ordered
-    Returns: A tuple (m, b, r2)
-
+    Returns: A tuple (m, b, r2) unless return_error is True, which will return only the r2 and error
     """
     slicer = start_end[:]
     slicer.sort()
@@ -43,13 +67,22 @@ def get_line(exes, whys, start_end):
     y = whys[start:end]
     m, b = np.polyfit(x, y, 1)
 
-    predictions = [func_linear(val, m, b) for val in x]
+    predictions = get_lin_values(x, m, b)
     r2 = get_r2(predictions, y)
 
-    return m, b, r2
+    if return_error:
+        return r2, sum_of_square_error(predictions, y)
+    else:
+        return m, b, r2
 
 
-def trawl(exes, whys, start, threshhold=0.8, step=-1):
+def plot_line(tup, from_x, to_x):
+    exes = [from_x,to_x]
+    whys = get_lin_values(exes,tup[0],tup[1])
+    plt.plot(exes,whys)
+
+
+def trawl(exes, whys, start, threshold=0.8, step=-1):
     """
     For a data series, starts at a point and begins fitting lines to an increasing number of points,
     terminating when the r2 value drops below a threshhold.
@@ -58,11 +91,10 @@ def trawl(exes, whys, start, threshhold=0.8, step=-1):
         exes: the x values in the series
         whys: the y values in the series
         start: the start index
-        threshhold: the r2 threshhold
+        threshold: the r2 threshold
         step: The increment value for traversing the data
 
     Returns: The index of the last point that meets the threshhold criteria
-
     """
 
     r2 = 1
@@ -74,7 +106,7 @@ def trawl(exes, whys, start, threshhold=0.8, step=-1):
         ind = start + step
 
     i = 0
-    while r2 > threshhold:
+    while r2 > threshold:
         ind += step
         if ind < 0 or ind > len(exes)-1:
             break
@@ -88,3 +120,110 @@ def trawl(exes, whys, start, threshhold=0.8, step=-1):
         res = [ind-step, start]
     res.sort()
     return res
+
+
+def split_segment(exes, whys, segment):
+    """
+    Takes a segment of a data series and splits it into two segments, where the new segments have optimal
+    linear fits based on minimizing the sum of the squared errors
+
+    Args:
+        exes: the x values in the series
+        whys: the y values in the series
+        segment: a list of len 2 indicating the start (inclusive) and end (exclusive) of the subset
+
+    Returns: a tuple with two sublists containing the start and end of each new segment and r2
+    """
+    #print(f'Splitting {segment}')
+    if segment[1] - segment[0] == 3:
+        seg1 = [segment[0], segment[0]+2]
+        seg2 = [segment[1]-2, segment[1]]
+        r1, e1 = get_line(exes, whys, seg1, return_error=True)
+        r2, e2 = get_line(exes, whys, seg2, return_error=True)
+
+        win_r1, win_r2 = r1, r2
+        win_segs = [[seg1[0], seg1[1], win_r1], [seg2[0], seg2[1], win_r2]]
+
+        #print(f'Split result: {win_segs}. Obligate result.')
+        return win_segs
+
+    win_r1, win_r2 = 0, 0
+    win_e1, win_e2 = -1, -1
+    win_segs = []
+    for i in range(segment[0]+2, segment[1]-1):
+        seg1 = [segment[0], i]
+        seg2 = [i-1, segment[1]]
+
+        r1, e1 = get_line(exes, whys, seg1, return_error=True)
+        r2, e2 = get_line(exes, whys, seg2, return_error=True)
+        #print(f'errors: {e1, e2}')
+        if 1/sum([e1, e2]) > 1/sum([win_e1, win_e2]):
+            #print(f'Winner')
+            win_r1, win_r2 = r1, r2
+            win_e1, win_e2 = e1, e2
+            win_segs = [[seg1[0], seg1[1], win_r1], [seg2[0], seg2[1], win_r2]]
+    #print(f'Split result: {win_segs}. Errors: {win_e1, win_e2}')
+    return win_segs
+
+
+def flatten(x):
+    ''' Creates a generator object that loops through a nested list '''
+    # First see if the list is iterable
+    try:
+        it_is = iter(x)
+    # If it's not iterable return the list as is
+    except TypeError:
+        yield x
+    # If it is iterable, loop through the list recursively
+    else:
+        for i in it_is:
+            for j in flatten(i):
+                yield j
+
+
+def regroup(x, n):
+    """
+    Turns a flat list into a list of lists with sublength n
+    Args:
+        x: flat list
+        i: sublist len
+
+    Returns: list of lists
+
+    """
+    i = 0
+    new_list = []
+    while i < len(x):
+        new_list.append(x[i:i + n])
+        i += n
+    return new_list
+
+
+def linear_recurse(exes, whys, threshold=0.8, segments=None):
+    """
+    Recursively breaks a data series into segments until the r2 for all segments exceeds the threshold.
+
+    Args:
+        exes: the x values in the series
+        whys: the y values in the series
+        threshold: the r2 threshold to be exceeded
+        segments: an optional list of lists that indicate the segment slices to start with and corresponding r2
+
+    Returns: a tuple of lists, where each list is the start (incl), end (excl) and r2 of each segment
+    """
+
+    if segments is None:
+        start_end = [0, len(exes)]
+        segments = [[start_end[0], start_end[1], get_line(exes, whys, start_end)[2]]]
+
+    #print(f'New segs: {segments}')
+    segments = [split_segment(exes,whys,seg) if seg[2] < threshold else seg for seg in segments]
+
+    flattened = [i for i in flatten(segments)]
+    segments = regroup(flattened, 3)
+    print(f'Segments: {segments}')
+    if all(seg[2] >= threshold for seg in segments):
+        print(f'Returning. Final: {segments}')
+        return segments
+    else:
+        return linear_recurse(exes, whys, threshold=threshold, segments=segments)
--- test.py
+++ test.py
@@ -10,20 +10,39 @@ from compression import *
 random.seed(2)
 np.random.seed(2)
 
-exes = [round(random.random()*100, 3) for i in range(0,200)]
-exes.sort()
 
+"""
+#150, 0.01, 0.8
+def func(x):
+    y = .2*math.sin(.1*x) + 0.023*x + math.sin(.002*x)\
+        - 0.0018*x**1.6 + 0.00000002*(x-100)**4\
+        + .05*math.sin(.5*x)\
+        + .4*math.cos(0.03*x+2)
+    y += np.random.normal(0,noise)
+    return y
+"""
+
+n = 100
+noise = 0.1
+thresh = 0.7
 
 def func(x):
     y = .2*math.sin(.1*x) + 0.023*x + math.sin(.002*x)\
         - 0.0018*x**1.6 + 0.00000002*(x-100)**4\
-        + .05*math.sin(.5*x)
-    y += np.random.normal(0,.0001)
+        + .05*math.sin(.5*x)\
+        + .4*math.cos(0.03*x+2)
+    y += np.random.normal(0,noise)
     return y
+
+####
+
+exes = [round(random.random()*100, 3) for i in range(0,n)]
+exes.sort()
 
 
 whys = [func(x) for x in exes]
 
+"""
 lin_end = trawl(exes, whys, 114, 0.6, -1)
 
 val_range = lin_end
@@ -31,8 +50,25 @@ m, b, r2 = get_line(exes, whys, val_range)
 
 nex = [exes[i] for i in val_range]
 ny = [func_linear(i, m, b) for i in nex]
+"""
+
+res = linear_recurse(exes, whys, thresh, None)
+print(f'Result: {res}')
+
+starts = [i[0] for i in res]
+ends = [i[1] for i in res]
+r2s = [i[2] for i in res]
+
+ses = [list(i) for i in zip(starts,ends)]
 
 plt.figure()
 plt.plot(exes, whys)
-plt.plot(nex, ny)
+for s in ses:
+    plt.plot(exes[s[0]:s[1]], whys[s[0]:s[1]])
+
+plt.plot(exes[32:100], whys[32:100])
+se = [32,100]
+l = get_line(exes,whys,se)
+plot_line(l, se[0], se[1])
+
 plt.show()
