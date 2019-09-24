3e9bdb09a354d302387ce10d831dd3723502cae6
rsjones94 <rsajones94@gmail.com>
2019-09-24 16:43:24 -0500

    no message

--- processing.py
+++ processing.py
@@ -13,70 +13,33 @@ from raster_extraction import *
 from date_extraction import *
 
 #out_loc = r'E:\hurricane\station_data\modified'
-stations_parent = r'E:\hurricane\station_data\modified'
+stations_parent = r'D:\SkyJones\hurricane\station_data\modified'
 station_files = os.listdir(stations_parent)
+
+sf = r'D:\SkyJones\hurricane\dates\hurricane_data_dates.txt'
+sef = r'D:\SkyJones\hurricane\station_nos'
+gauge_dates = relate_gauges_to_storms(sf, sef)
+gauges = list(gauge_dates.keys())
+gauge_files = [os.path.join(stations_parent, gauge+'.csv') for gauge in gauges]
 print('Reading station data in')
 
-station_dfs = {station[:-4]:clean_read(os.path.join(stations_parent,station)) for station in station_files}
-
-has_rain = []
-for gauge, df in station_dfs.items():
-    rain = df.Rain.dropna()
-    if not rain.empty:
-        has_rain.append(gauge)
-
-sf = r'E:\hurricane\dates\hurricane_data_dates.txt'
-sef = r'E:\hurricane\station_nos'
-gauge_dates = relate_gauges_to_storms(sf, sef)
-has_storm = list(gauge_dates.keys())
-
+station_dfs = {gauge:clean_read(file) for gauge,file in zip(gauges,gauge_files)}
 modded_gauge_dates = onsets_by_rain(gauge_dates,station_dfs)
 
 
-storm_and_rain = [i for i in has_storm if i in has_rain]
-rain_but_no_storm = [i for i in has_rain if i not in has_storm]
-storm_but_no_rain = [i for i in has_storm if i not in has_rain]
+# gauge = '02160700' # chop at index 4293 for PH
+gauge = '02198950'
+param = 'PH'
 
+d = station_dfs[gauge]
+plt.figure()
+plt.plot(d.index,d[param])
+plt.xlabel('Date')
+plt.ylabel(param)
+plt.title(gauge)
+plt.show()
 
-"""
-plt.ioff()
-for i in storm_and_rain:
-    gauge = i
-    df = station_dfs[gauge]
-
-    indices = []
-    for storm in gauge_dates[gauge]:
-        date = gauge_dates[gauge][storm]
-        mask = df['Date'] == date
-        storm_row = df[mask]
-        storm_ind = int(storm_row.index[0])
-        indices.append(storm_ind)
-
-
-    y = df.Rain
-    x = df.index
-
-    plt.figure()
-    plt.plot(x,y)
-
-    for i in indices:
-        plt.axvline(i, color='red')
-
-    new_date, ind = onset_by_rain(date, df, window=5, rain_threshold=5)
-    plt.axvline(ind, color='orange', linestyle='dashed')
-    plt.xlim(ind-10,ind+10)
-    plt.title(gauge)
-    #plt.show()
-    out = os.path.join(r'E:\hurricane\test_plots', f'{gauge}.pdf')
-    plt.savefig(out)
-    plt.close()
-
-plt.ion()
-"""
-
-
-
-
+# detrending DO
 """
 val_err, type_err = [], []
 n = len(station_dfs)
@@ -93,9 +56,10 @@ for i,(gauge, df) in enumerate(station_dfs.items()):
         print(f'ValueError on {gauge}')
         station_dfs[gauge]['DO Detrend'] = np.nan
         val_err.append(gauge)
+"""
 
-
-
+# adding PRISM rain data
+"""
 gauge_file = r'E:\hurricane\station_coords.csv'
 
 col_names = pd.read_csv(gauge_file, nrows=0).columns
@@ -127,9 +91,10 @@ for gauge_no, gauge_df in station_dfs.items():
         pass
 
 station_dfs = {station:df.reset_index(level='Date') for station, df in station_dfs.items()}
+"""
 
-
-
+# writing
+"""
 for gauge,df in station_dfs.items():
     print(f'On {gauge}')
     out_name = os.path.join(out_loc,f'{gauge}.csv')
