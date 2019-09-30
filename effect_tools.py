f375f02d48f5b19ccf67d086ced8bcdbb15009bc
Sky Jones <rsajones94@gmail.com>
2019-09-29 23:11:50 -0500

    improve return forcing

--- effect_tools.py
+++ effect_tools.py
@@ -9,13 +9,14 @@ from read import clean_read
 from detrend import *
 
 choice_param = 'Turb Detrend'
-choice_gauge = '07263296'
+choice_gauge = '02292900'
 
 results_folder = r'E:\hurricane\results'
 data_folder = r'E:\hurricane\station_data\modified'
 
 def get_effect(data, param, mean, stddev, start_index, lag=3, effect_type=1,
-               dropthrough=[0,0], force_completion=None, max_effect=365, max_droput=5):
+               returning_gap=0, dropthrough=(0,0), forcing=(None,None),
+               max_effect=365, max_droput=5):
     """
     For a given parameter, finds the time it takes for the time series to return to normalcy
     after a peturbation
@@ -29,27 +30,37 @@ def get_effect(data, param, mean, stddev, start_index, lag=3, effect_type=1,
         lag: the number of days allowed for an effect to begin. Minimum is 1
         effect_type: the INITIAL expected effect of the peturbation. 1 indicates a positive effect, -1
                      indicates a negative effect
-        dropthrough: A list (not a tuple) indicating the number of dropthroughs allowed and the number of days
+        returning_gap: number of days where an increasing effect is allowed to reverse trend
+                          before it is considered to be on its reverse trend
+        dropthrough: A list or tuple indicating the number of dropthroughs allowed and the number of days
                      the time series is allotted to drop through before being considered terminated
-        force_completion: the number of days a returning trend can be reversed before it is forced to
+        forcing: a tuple of 1.the number of days a returning trend can be reversed before it is forced to
                           return by calculating the best fit line for the last three returning days and
                           calculating the date of intersection. This allows an effect window to be
                           estimated even when additional storms/forcing effects follow the initial
                           peturbation. Default is None, which will never force a completion.
+                          2. the number of points to include in the forcing slope fit line
         max_effect: the maximum number of days an effect can continue before being terminated
         max_dropout: number of continues no signals before mandatory termination
 
     Returns:
         A list with two parts. The first is a list of the start and end indices of the effect
         (or None, if there was no effect). The second is list, (days_above, days_below, days_between,
-        termination_type, forcing_start, forcing_slope). termination_type can be "natural" or "forced".
+        termination_type, forcing_start, forcing_slope). termination_type can be "natural", "forced",
+        None or 'dropout'
         If not forced, forcing_start and forcing_slope will be None.
     """
     returner = [[None, None], [0, 0, 0, 'natural', None, None]]
 
+    force_completion = forcing[0] # number of days to regress before completion is forced
+    force_history = forcing[1]
+
+    dropthrough = (dropthrough[0], dropthrough[1])
+
     comp_dict = {1:greater,-1:lesser}
 
-    exes = np.array(data.index)
+    orig = np.array(data.index)
+    exes = np.array(pd.Series(orig).interpolate(limit_direction='both'))
     whys = np.array(data[param])
 
     low = mean - stddev
@@ -79,17 +90,39 @@ def get_effect(data, param, mean, stddev, start_index, lag=3, effect_type=1,
 
     i -= 1
     is_returning = False
+    nan_count = 0
+    ret_gap_count = 0
     while True:
         i += 1
+
         if i > (i + max_effect):
             returner[1][3] = 'max_effect'
+
+        if np.isnan(orig[i]):
+            nan_count += 1
+            if nan_count > max_droput:
+                returner[1][3] = 'dropout'
+                break
+        else:
+            nan_count = 0
+
         last_val = whys[i-1]
         val = whys[i]
-        if comp_dict[effect_type](last_val,val): # checking to see if the data has started going back to pre-peturbation
-            is_returning = True
+
+        if comp_dict[effect_type](last_val,val) and not is_returning: # checking to see if the data has started going back to pre-peturbation
+            ret_gap_count += 1
+            print('retgap')
+            if ret_gap_count > returning_gap:
+                print('returning')
+                is_returning = True
+                ret_gap_count = 0
+        elif not is_returning:
+            ret_gap_count = 0
+
         if is_returning:
 
             if comp_dict[effect_type](comp_val,val): # check to see if we've returned to normalcy
+
                 if dropthrough[0] == 0: # if no dropthroughs left then we're done
                     break
                 else:
@@ -105,14 +138,33 @@ def get_effect(data, param, mean, stddev, start_index, lag=3, effect_type=1,
                     effect_type = -effect_type
                     is_returning = False
 
-
             elif force_completion and comp_dict[effect_type](val,last_val):
-                # check to see if the data is moving away from pre-pet again, force_completion is numeric
-                if days_to_return(exes, i, max_nan=max_droput) <= force_completion: # if we return in time
-                    pass # do nothing
-                else: # force completion
-                    raise NotImplementedError('Completion forcing not implemented yet')
+                # check to see if the data is moving away from pre-pet again
+                # assuming force_completion is numeric
 
+                print('Force completion active')
+                print(f'Func {comp_dict[effect_type]}, vals {val,last_val}. Ind {i}')
+                dn = days_to_return(whys, i-1, func=comp_dict[-effect_type], max_nan=max_droput)
+                if dn <= force_completion: # if we return in time
+                    if last_val > high:
+                        returner[1][0] += (dn-1)
+                    if last_val < low:
+                        returner[1][1] += (dn-1)
+                    i += (dn-1)
+                else: # force completion
+                    print(f'Forcing completion')
+                    ind, days_to_force, slope = forced_return(exes, whys, i-1, normalcy, history=force_history)
+                    print(f'Completion forced at {ind} from {i-1}. Takes {days_to_force} days. Slope: {slope}')
+                    returner[1][3] = 'forced'
+                    returner[1][4] = i-1
+                    returner[1][5] = slope
+                    to_add = days_to_force - 1
+                    if last_val > high:
+                        returner[1][0] += to_add
+                    if last_val < low:
+                        returner[1][1] += to_add
+                    i = ind
+                    break
 
         if val > high:
             returner[1][0] += 1
@@ -133,9 +185,62 @@ def within(a,b):
     return b[1] > a > b[0]
 
 
-def days_to_return(exes, i, max_nan=0):
+def forced_return(exes, whys, i, window, history=3):
     """
-    Returns the days for a series to return to above/below the indexed value
+    Gives the index of a forced return and the slope of the return
+
+    Args:
+        exes: x vals
+        whys: y vals
+        i: index of the return begin
+        window: the min and max of the return window
+        history: number of points to include in the best fit
+
+    Returns:
+        tuple (index_of_return, days_to_return, slope)
+
+    """
+
+    while True:
+        x = exes[(i-history+1):(i+1)]
+        y = whys[(i-history+1):(i+1)]
+        m, b = np.polyfit(x, y, 1)
+
+        if whys[i] > window[1] and m >= 0:
+            history -= 1
+        elif whys[i] < window[0] and m <= 0:
+            history -= 1
+        else:
+            break
+
+        if history == 1:
+            raise ValueError('Forced return impossible')
+
+    def lin_func(index, y=whys[i], anchor=i, slope=m):
+        r = y + (index-anchor)*slope
+        return r
+
+    if exes[i] > window[1]:
+        func = lesser
+        comp = window[1]
+    elif whys[i] < window[0]:
+        func = greater
+        comp = window[0]
+
+    val = whys[i]
+    n = 0
+    while not func(val,comp):
+        i += 1
+        n += 1
+        val = lin_func(index=i)
+        print(val)
+
+    return i, n, m
+
+
+def days_to_return(exes, i, func, max_nan=0):
+    """
+    Returns the number of days for a series to return to above/below the indexed value
 
     Args:
         exes: series of x vals
@@ -146,25 +251,27 @@ def days_to_return(exes, i, max_nan=0):
         num of days to return
 
     """
+    if func is lesser:
+        print('looking for when vals drop below comp')
+    elif func is greater:
+        print('looking for when vals rise above comp')
 
     initial = exes[i]
-    sec = exes[i+1]
-    if sec > initial:
-        func = lesser
-    elif sec < initial:
-        func = greater
 
     nas = 0
     n = 0
-    while nas <= max_nan:
-        i += 1
-        n += 1
-        val = exes[i]
-        if np.isnan(val):
-            nas += 1
-        elif func(val,initial):
-            break
-
+    try:
+        while nas <= max_nan:
+            i += 1
+            n += 1
+            val = exes[i]
+            print(f'Compare {val} to initial ({initial})')
+            if np.isnan(val):
+                nas += 1
+            elif func(val,initial):
+                break
+    except IndexError:
+        pass
     return n
 
 
@@ -238,7 +345,7 @@ low = mean - stddev
 high = mean + stddev
 
 (es, ee), stats = get_effect(data, choice_param, mean, stddev, start, lag=3, effect_type=1,
-               dropthrough=[0,0], force_completion=None, max_effect=365, max_droput=5)
+               returning_gap=2, dropthrough=[0,0], forcing=(5,4), max_effect=365, max_droput=5)
 
 plt.figure()
 
@@ -247,8 +354,22 @@ plt.plot(data.index,data[choice_param])
 plt.axvline(start, color='red')
 plt.axhline(high, color='orange')
 plt.axhline(low, color='orange')
-plt.axvline(es, color='green')
-plt.axvline(ee, color='blue')
+if stats[3] is not None:
+    plt.axvline(es, color='green')
+    plt.axvline(ee, color='blue')
+if stats[3] == 'forced':
+    x1 = stats[4]
+    x2 = ee
+    y1 = data[choice_param][stats[4]]
+    y2 = y1 + (x2-x1)*stats[5]
+
+    fx = [x1,x2]
+    fy = [y1,y2]
+
+    plt.plot(fx,fy,color='black', linestyle='dashed')
+
 plt.xlim(start-28,start+28)
+plt.title(f'Above: {stats[0]}, Below: {stats[1]}, Between: {stats[2]} \n'
+          f'Termination Type: {stats[3]}')
 
 plt.show()
